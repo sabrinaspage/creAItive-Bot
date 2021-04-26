01ad57605697b1267a1974a5d1eb59db306a458e
sabrinaspage <sabreyes01@gmail.com>
2021-04-26 14:14:43 -0400

    refactor

--- main.py
+++ main.py
@@ -12,19 +12,19 @@ import datetime
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-according_to_1 = "NY7 "
-according_to_2 = "CB$ "
-according_to_3 = "F0X "
-according_to_4 = "M$NBC "
-according_to_5 = "CMM "
-
-according_to = [according_to_1, according_to_2,
-                according_to_3, according_to_4, according_to_5]
+according_to = ["NYT ", "CBS ", "FOX ", "MSNBC ", "CNN ", "WSJ ", "DOW "]
 
 random.seed(time.time())
 
+SECONDS = 15
+
 
 def get_article(fil):
+    """
+    returns a list of all the available articles to extract
+    """
+    random.seed(time.time())
+
     a_file = open(fil, "r")
 
     list_of_lists = []
@@ -36,23 +36,36 @@ def get_article(fil):
 
 
 def random_date():
+    """
+    gets a random date to associate with the article
+    """
     random.seed(time.time())
     d = random.randint(1, int(time.time()))
     return f'({str(datetime.date.fromtimestamp(d).strftime("%Y-%m-%d"))}): '
 
 
 def get_random_str(main_str):
+    """
+    gets a random sentence from the article with the help of nltk
+    """
     random.seed(time.time())
     sentences = tokenize.sent_tokenize(main_str)
     random_sentence = random.choice(sentences)
+
+    # asserts that line can fit in tweet
     while len(random_sentence) < 30 or len(random_sentence) > 200:
         random_sentence = random.choice(sentences)
+
+    # quotes end with quotes
     if random_sentence.startswith('“'):
         random_sentence += '”'
     return random.choice(according_to) + random_date() + random_sentence
 
 
 def content_list(lst):
+    """
+    returns a list of sentences we want to tweet
+    """
     news_content = []
 
     for ex in lst:
@@ -64,6 +77,9 @@ def content_list(lst):
 
 
 def tweet(api):
+    """
+    tweets the articles every few seconds
+    """
     logger.info("Tweeting content")
     urls = get_article('the_onion.txt')
     lines = content_list(urls)
@@ -72,7 +88,7 @@ def tweet(api):
         try:
             api.update_status(line)
             logger.info("Tweeting!")
-            time.sleep(30)
+            time.sleep(SECONDS)
 
         except tweepy.TweepError as err:
             logger.error(err)
@@ -81,7 +97,8 @@ def tweet(api):
 def main():
     logger.info("Program start")
     api = create_api()
-    tweet(api)
+    while True:
+        tweet(api)
     logger.info("Program complete")
 
 
